ce14d13ccd3503c94c8bf072a076597cc05ae899
Sneha Gupta <90573650+Sgakgsgsg@users.noreply.github.com>
2023-12-08 09:14:09 +0000

    ok

---
+++ .devcontainer/01basics/memory.js
@@ -0,0 +1,6 @@
+// Stack(Primitive)-copy, Heap(Non Primitive)-refrence
+//sting interpolation using backtick concatinating 
+const name="sneha"
+const repocount =50
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
