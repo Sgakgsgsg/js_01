36d7bafe6c237aea594aa759c50aa3b64b093e4b
Sneha Gupta <90573650+Sgakgsgsg@users.noreply.github.com>
2023-12-09 13:10:50 +0000

    arr

--- .devcontainer/02array.js
+++ .devcontainer/02array.js
@@ -1,16 +1,24 @@
-//in javascript arrays are resizeable and also we can store any type of datatype
-// javascript creates shallow copies(whose properties share the same refrences)
-const myArr=[0,1,2,3,4]
-//const myArr=["monica","chandler","rachel"]
-const myArr2=new Array(1,2,3,4)
-console.log(myArr2[0]);
-myArr2.push(7);
-console.log(myArr2);
-//by using shift we can remove 0 index element
-//by using unshift we can add at 0 th index
-myArr2.unshift(9)
-myArr2.shift()
-console.log(myArr2);
+// //in javascript arrays are resizeable and also we can store any type of datatype
+// // javascript creates shallow copies(whose properties share the same refrences)
+// const myArr=[0,1,2,3,4]
+// //const myArr=["monica","chandler","rachel"]
+// const myArr2=new Array(1,2,3,4)
+// console.log(myArr2[0]);
+// myArr2.push(7);
+// console.log(myArr2);
+// //by using shift we can remove 0 index element
+// //by using unshift we can add at 0 th index
+// myArr2.unshift(9)
+// myArr2.shift()
+// console.log(myArr2);
 //join combine kr raha hai or string mei convert kar raha hai
 //slice:- range ka last wala digit add nhi hoga 
 //splice :- add hoga
+//when you have to merge two arrays in  a array we will use concat and also spread
+// const arr=[....arr1,..arr2]
+//console.log(arr);
+
+// const another_arr=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_arr=another_arr.flat(Infinity)
+// console.log(real_arr);
+//console.log(Array.from("Sneha"))
