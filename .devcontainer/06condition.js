7dae7279ebca869e41346844ead97cb39045f665
Sneha Gupta <90573650+Sgakgsgsg@users.noreply.github.com>
2023-12-11 17:09:30 +0000

    a5

---
+++ .devcontainer/06condition.js
@@ -0,0 +1,45 @@
+// //---------- switch case----------
+// const month=3
+// switch(month ){
+//      case 1:
+//         console.log("January")
+//         break;
+//      case 2:
+//         console.log("January")
+//         break;
+//      case 3:
+//         console.log("January")
+//         break;
+//         //shift alt down arrow
+    
+//      default:
+//         break;   
+//}
+
+// const userEmail=[]
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("dont have user email");
+
+// }
+// empty string =false;
+// empty array= true;
+
+
+// falsy value
+  //false,0,-0,BigInt 0n,"",null,undefined,NaN
+  // truthy values
+  // "0","false"," ",[],{},function(){}
+
+  // //---------- Nullish Coalescing Operator(??) : null undefined-------
+  // let val1;
+  // // val1=5??10
+  // val1 =null??10// 10
+  // val1= undefined ??15//15
+  // val1 =null ?? 10?? 20//10
+  // console.log(val1)
+  //-----------Ternary Operator------
+  const ip=100
+  ip<=80 ? console.log("less than 80") : console.log("more than 80")
